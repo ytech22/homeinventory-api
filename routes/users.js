fbf1a3464a5fb7714ad3b0903a9b255de6564147
martinwachira <martinnwachira@gmail.com>
2022-04-25 23:52:12 +0300

    impl get users

--- routes/users.js
+++ routes/users.js
@@ -13,15 +13,21 @@ router.post("/", async (req, res, next) => {
       "INSERT INTO users (username, password ) VALUES ($1, $2) RETURNING *",
       [username, password]
     );
-    res.json(newUser);
+    res.json(newUser.rows);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/", async (req, res, next) => {
+  // res.send("respond with a resource");
+  try {
+    const users = await pool.query("SELECT * FROM users");
+    res.json(users.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 module.exports = router;
