2e9911cc7b71e5333cfa226de738e4b23e7a5462
martinwachira <martinnwachira@gmail.com>
2022-05-17 21:23:20 +0300

    resolve conf

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Home Inventory" });
+  res.render("index", { title: "Home Inventory V0" });
 });
 
 module.exports = router;
